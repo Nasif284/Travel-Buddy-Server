b8fc3b2405677bf67649689d079a520f73f0b4aa
Muhammad Nasif <nasifhima@gmail.com>
2026-08-27 16:38:52 +0530

    test:testing ci/cd pipeline

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,6 @@ async function bootstrap(): Promise<void> {
     console.error('[PostgreSQL] Failed to connect:', err);
     process.exit(1);
   }
-
   try {
     console.log('[Server] Pinging Redis...');
     await redis.ping();
